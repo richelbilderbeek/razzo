c86d34e1f0af3deb167b6857fbe3933da5ef645b
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-25 14:00:05 +0200

    Use mbd namespace

--- R/simulate_alignments.R
+++ R/simulate_alignments.R
@@ -17,9 +17,9 @@ BD.Nct <- function(lambda, mu, t, N0 = 2, age) {
   tt  <- t
   TT  <- abs(age)
   Lt  <- exp((lambda - mu) * tt)
-  Nct <- N0 * Lt * P_t1_t2(lambda = lambda, mu = mu, t1 = tt, t2 = TT) * 
+  Nct <- N0 * Lt * P_t1_t2(lambda = lambda, mu = mu, t1 = tt, t2 = TT) *
     (P_t1_t2(lambda = lambda, mu = mu, t1 = 0, t2 = TT))^-1#new version from etienne & rosindell 2012, formula #15
-  
+
   return(Nct)
 }
 
@@ -28,7 +28,7 @@ BD.Nct <- function(lambda, mu, t, N0 = 2, age) {
 #' @inheritParams default_params_doc
 #' @export
 BD.Nmutations <- function(lambda, mu, age, N0 = 2, sequence_length = 1000, mutation_rate = 1/age) {
-  
+
   age <- abs(age)
   ft  <- function(t) {mbd::BD.Nct(t, lambda = lambda, mu = mu, N0 = N0, age = age)}
   Nmutations <- mutation_rate * sequence_length * stats::integrate(f = ft, lower = 0, upper = age)[[1]]
@@ -39,13 +39,13 @@ BD.Nmutations <- function(lambda, mu, age, N0 = 2, sequence_length = 1000, mutat
 #' Does something D
 #' @inheritParams default_params_doc
 #' @export
-BD.infer.lambda.from.mutations <- function(Nsubs, 
-                                           mbd.lambda, 
-                                           mu, 
-                                           age, 
-                                           N0 = 2, 
-                                           sequence_length = 1000, 
-                                           mutation_rate = 1/age, 
+BD.infer.lambda.from.mutations <- function(Nsubs,
+                                           mbd.lambda,
+                                           mu,
+                                           age,
+                                           N0 = 2,
+                                           sequence_length = 1000,
+                                           mutation_rate = 1/age,
                                            Nsteps = 40) {
   lavec <- seq((min.lambda <- 0.5 * mbd.lambda), (max.lambda <- 6 * mbd.lambda), by = abs(max.lambda - min.lambda)/(Nsteps))
   NN <- rep(NA, length(lavec))
@@ -56,9 +56,9 @@ BD.infer.lambda.from.mutations <- function(Nsubs,
   }
   md <- stats::lm(log(NN) ~ lavec)
   fit.test <- stats::nls(
-    NN ~ b*exp(m*lavec), 
+    NN ~ b*exp(m*lavec),
     start = list(
-      m = stats::coef(md)[2], 
+      m = stats::coef(md)[2],
       b = stats::coef(md)[1]
     )
   )
@@ -78,7 +78,7 @@ alignments_comparison_single <- function(sim_phylo,
                                          sequence_length = 1e+03,
                                          max_repetitions = 10,
                                          mutation_rate = 1/max(abs(ape::branching.times(sim_phylo)))) {
-  
+
   #settings
   age <- max(abs(ape::branching.times(sim_phylo)))
   esses <- 1
@@ -101,20 +101,20 @@ alignments_comparison_single <- function(sim_phylo,
       verbose = FALSE,
       beast_jar_path = beastier::get_default_beast2_jar_path()
     )
-    
+
     #check esses
     esses2 <- tracerer::calc_esses(pirouette.out$estimates, sample_interval = sample_interval) #effective sample size: has to be at least 200, try not to be over 1000 otherwise you're overkilling (try to stick to 1/5 rule of thumb)
     esses  <- esses2$posterior
     quality_ratio <- esses/(nrow(pirouette.out$estimates))
-    
+
     #this will apply at the next while run if you don't fulfill the quality condition
     chain_length    <- 2 * chain_length
     sample_interval <- 2 * sample_interval
-    
+
     repetitions <- repetitions + 1
     if (repetitions >= max_repetitions) {break}
   }
-  
+
   #calculating nLTT statistics
   nLTT.diff <- rep(NA, length(pirouette.out$trees))
   for (i in 1:length(pirouette.out$trees))
@@ -124,8 +124,8 @@ alignments_comparison_single <- function(sim_phylo,
   mean.nLTT <- mean(nLTT.diff); mean.nLTT
   std.nLTT  <- sqrt(stats::var(nLTT.diff)); std.nLTT
   df.nLTT   <- data.frame(diff = nLTT.diff)
-  
-  return(list(alignment = pirouette.out$alignment, 
+
+  return(list(alignment = pirouette.out$alignment,
               trees = pirouette.out$trees,
               estimates = pirouette.out$estimates,
               nLTT = df.nLTT))
@@ -147,30 +147,30 @@ alignments_comparison_multiple <- function(
   mutation_rate = 1/age
 ) {
   ..count.. <- NULL; rm(..count..) # nolint, fixes warning: no visible binding for global variable
-  
-  
+
+
   #setting
   soc <- 2 #it has to start with a crown to use pirouette
-  
+
   #set filename to save results
   dir_name <- paste0(dirname(getwd()),"//results//nLTT//"); suppressWarnings(dir.create(dir_name))
   sim_pars_string <- gsub(", ", "-", toString(sim_pars))
   folder_name <- paste0(dir_name, sim_pars_string)
   suppressWarnings(dir.create(folder_name))
   j <- 1; while ((file_name <- paste0("nLTT", j)) %in% list.files(folder_name)) {j <- j + 1}
-  
+
   #sim dataset
   mbd.estimates   <- mbd.trees <- mbd.alignment <- mbd.nLTT <- vector("list", max_sims)
   BD.simulations  <- BD.estimates  <- BD.trees  <- BD.alignment  <- BD.nLTT  <- vector("list", max_sims)
   Nsubstitutions  <- rep(NA, (max_sims2 <- 100 * max_sims))
   for (s in 1:max_sims)
   {
-    mbd.simulation <- mbd_sim(pars = sim_pars,
+    mbd.simulation <- mbd::mbd_sim(pars = sim_pars,
                                     soc = soc,
                                     age = age,
                                     cond = cond,
                                     tips_interval = tips_interval)
-    
+
     full_tree           <- mbd.simulation$tas #; graphics::plot(full_tree)
     total_branch_length <- sum(full_tree$edge.length) # total branch length
     Nsubstitutions[s]   <- sequence_length * mutation_rate * total_branch_length
@@ -180,13 +180,13 @@ alignments_comparison_multiple <- function(
                                             sample_interval = sample_interval,
                                             sequence_length = sequence_length,
                                             mutation_rate = mutation_rate)
-    
+
     mbd.nLTT[[s]]      <- mbd.out$nLTT
     mbd.alignment[[s]] <- mbd.out$alignment
     mbd.trees[[s]]     <- mbd.out$trees
     mbd.estimates[[s]] <- mbd.out$estimates
   # }
-  
+
   # BD.lambda <- BD.infer.lambda.from.mutations(Nsubs = mean(Nsubstitutions),
     BD.lambda <- BD.infer.lambda.from.mutations(Nsubs = Nsubstitutions[s],
                                                 mbd.lambda = sim_pars[1],
@@ -196,52 +196,52 @@ alignments_comparison_multiple <- function(
                                                 sequence_length = sequence_length,
                                                 mutation_rate = mutation_rate,
                                                 Nsteps = 40)
-  
+
   # for (s in 1:max_sims)
   # {
-    BD.simulations[[s]] <- mbd_sim(pars = c(BD.lambda, sim_pars[2], 0, 0),
+    BD.simulations[[s]] <- mbd::mbd_sim(pars = c(BD.lambda, sim_pars[2], 0, 0),
                                          soc = soc,
                                          age = age,
                                          cond = cond,
                                          tips_interval = tips_interval)
-    
+
     BD.out <- alignments_comparison_single(sim_phylo = BD.simulations[[s]]$tes,
                                            chain_length = chain_length,
                                            sample_interval = sample_interval,
                                            sequence_length = sequence_length)
-    
+
     BD.nLTT[[s]]      <- BD.out$nLTT
     BD.alignment[[s]] <- BD.out$alignment
     BD.trees[[s]]     <- BD.out$trees
-    BD.estimates[[s]] <- BD.out$estimates 
+    BD.estimates[[s]] <- BD.out$estimates
   }
-  
+
   #roba
   mbd.nLTT.total <- unname(unlist(mbd.nLTT))
   BD.nLTT.total  <- unname(unlist(BD.nLTT))
   df.mbd.nLTT <- data.frame(mbd.nLTT = mbd.nLTT.total)
   df.BD.nLTT  <- data.frame(BD.nLTT  = BD.nLTT.total)
-  
+
   mbd.mean <- mean(df.mbd.nLTT$mbd.nLTT)
   mbd.std  <- sqrt(stats::var(df.mbd.nLTT$mbd.nLTT))
   BD.mean  <- mean(df.BD.nLTT$BD.nLTT)
   BD.std   <- sqrt(stats::var(df.BD.nLTT$BD.nLTT))
-  
+
   #show results
-  mbd.nLTT.plot <- ggplot2::ggplot(df.mbd.nLTT, ggplot2::aes(x = mbd.nLTT)) + 
-    ggplot2::geom_histogram(ggplot2::aes(y = ..count..), colour = "darkblue", fill = "lightblue", bins = 40) + 
+  mbd.nLTT.plot <- ggplot2::ggplot(df.mbd.nLTT, ggplot2::aes(x = mbd.nLTT)) +
+    ggplot2::geom_histogram(ggplot2::aes(y = ..count..), colour = "darkblue", fill = "lightblue", bins = 40) +
     ggplot2::geom_vline(ggplot2::aes(xintercept = mean(mbd.nLTT)), color = "red", linetype = "dashed", size = 1) +
     ggplot2::ggtitle(paste0("nLTT statistics for a mbd tree compared \nwith tree posterior generated from alignments and BD prior \nmean = ", signif(mbd.mean, digits = 2), ", std = ", signif(mbd.std, digits = 2))) +
     ggplot2::xlab("nLTT value")
   graphics::plot(mbd.nLTT.plot)
-  
-  BD.nLTT.plot <- ggplot2::ggplot(df.BD.nLTT, ggplot2::aes(x = BD.nLTT)) + 
-    ggplot2::geom_histogram(ggplot2::aes(y = ..count..), colour = "darkblue", fill = "lightblue", bins = 40) + 
+
+  BD.nLTT.plot <- ggplot2::ggplot(df.BD.nLTT, ggplot2::aes(x = BD.nLTT)) +
+    ggplot2::geom_histogram(ggplot2::aes(y = ..count..), colour = "darkblue", fill = "lightblue", bins = 40) +
     ggplot2::geom_vline(ggplot2::aes(xintercept = mean(BD.nLTT)), color = "red", linetype = "dashed", size = 1) +
     ggplot2::ggtitle(paste0("nLTT statistics for a BD tree compared \nwith tree posterior generated from alignments and BD prior \nmean = ", signif(BD.mean, digits = 2), ", std = ", signif(BD.std, digits = 2))) +
     ggplot2::xlab("nLTT value")
   graphics::plot(BD.nLTT.plot)
-  
+
   #save results
   save(mbd.nLTT = df.mbd.nLTT,
        mbd.alignment = mbd.alignment,
@@ -252,15 +252,15 @@ alignments_comparison_multiple <- function(
        BD.trees = BD.trees,
        BD.estimates = BD.estimates,
        file = paste0(folder_name,"//" ,file_name))
-  
+
   grDevices::png(filename = paste0(folder_name, "//" ,file_name, "_mbd_plot.png"))
   graphics::plot(mbd.nLTT.plot)
   grDevices::dev.off()
-        
+
   grDevices::png(filename = paste0(folder_name, "//" ,file_name, "_BD_plot.png"))
   graphics::plot(BD.nLTT.plot)
   grDevices::dev.off()
-  
+
   return(list(mbd.nLTT = df.mbd.nLTT, BD.nLTT = df.BD.nLTT))
 }
 
--- scripts/mbd_prototypes.R
+++ scripts/mbd_prototypes.R
@@ -29,14 +29,14 @@
 #MAIN
 mbd_loglik0 <- function(pars, brts, soc = 2, cond = 0, tips_interval = c(0,Inf), missnumspec = 0,
                         safety_threshold = 1e-4,methode = "expo", debug_check = 0,alpha = 20){
-  
+
   #Optional stuff that I might need to run the program one line at the time:
   #brts=sim_data[[1]];missnumspec=0;pars=sim_pars;missing_interval=c(1,Inf)
-  
+
   #BASIC SETTINGS AND CHECKS
   lambda=pars[1]; mu=pars[2]; q=pars[3]; min_tips=tips_interval[1]; max_tips=tips_interval[2]; abstol=1e-16; reltol=1e-10;
   starting_alpha = alpha
-  
+
   condition1 = ( any(is.nan(pars))!=0 | any(is.infinite(pars))!=0 )
   condition2 = ( mu<0 | lambda<=0+safety_threshold | q<=0+safety_threshold | q>=1-safety_threshold ) # mu>=lambda |
   if (condition1 | condition2){loglik = -Inf}
@@ -44,19 +44,19 @@ mbd_loglik0 <- function(pars, brts, soc = 2, cond = 0, tips_interval = c(0,Inf),
   else{
     Pc=-1
     while (Pc<0 && alpha <= max(starting_alpha,80)){
-      
+
       #ADJUSTING DATA
       data=brts2time_intervals_and_births(brts)
       time_intervals=c(0,data$time_intervals)
       births=c(0,data$births)
-      
+
       #SET UP
       N0 = soc #number of starting species
       k_interval = N0 + cumsum(births)
       max_k = max(k_interval)
       max_number_of_species = alpha*max_k; #alpha is the proportionality factor between max_k and the edge of the matrix
       nvec=0:max_number_of_species
-      
+
       #SETTING INITIAL CONDITIONS (there's always a +1 because of Q0)
       Qi=c(1,rep(0,max_number_of_species))
       Qt=matrix(0,ncol = (max_number_of_species+1), nrow = length(time_intervals))
@@ -66,20 +66,20 @@ mbd_loglik0 <- function(pars, brts, soc = 2, cond = 0, tips_interval = c(0,Inf),
       t=2  #t is starting from 2 so everything is ok with birth[t] and time_intervals[t] vectors
       C=rep(1,(length(time_intervals))); D=C
       logB=0;
-      
+
       #EVOLVING THE INITIAL STATE TO THE LAST BRANCHING POINT
       while ( t<length(time_intervals) ){
-        
+
         #Applying A operator
         transition_matrix=create_A0(max_number_of_species = max_number_of_species,lambda = lambda,mu = mu,q = q,k = k)
         Qt[t,]=A_operator(Q = Qt[(t-1),],transition_matrix = transition_matrix,time_interval = time_intervals[t],precision = 50L,methode=methode,A_abstol=abstol,A_reltol=reltol)
         if (methode!="sexpm"){Qt[t,]=mbd:::negatives_correction(Qt[t,],pars)} #it removes some small negative values that can occurr as bugs from the integration process
-        
+
         #Applying C operator (this is a trick to avoid precision issues)
         if (debug_check==1){print(head(Qt[t,]))}
         C[t]=1/(sum(Qt[t,]))
         Qt[t,]=Qt[t,]*C[t]
-        
+
         #Applying B operator
         B <- create_B0(max_number_of_species = max_number_of_species, q = q, k = k, b = births[t])
         # B[(row(B)>(2*col(B)+k-births[t])) | col(B)>row(B) ]=0 #this is a constrain due to maximum number of speciations being (2*n+k-b); probably it is redundant
@@ -87,51 +87,51 @@ mbd_loglik0 <- function(pars, brts, soc = 2, cond = 0, tips_interval = c(0,Inf),
         Qt[t,]=(B %*% Qt[t,])
         if (methode!="sexpm"){Qt[t,]=mbd:::negatives_correction(Qt[t,],pars)}
         logB = logB + log(lambda) + lchoose(k,births[t]) + births[t]*log(q)
-        
+
         #Applying D operator (this works exactly like C)
         if (debug_check==1){print(head(Qt[t,]))}
         D[t]=1/(sum(Qt[t,]))
         Qt[t,]=Qt[t,]*D[t]
-        
+
         #Updating running parameters
         k=k+births[t]
         t=t+1
       }
-      
+
       #Applying A operator from the last branching time to the present
       transition_matrix=create_A0(max_number_of_species = max_number_of_species,lambda = lambda,mu = mu,q = q,k = k)
       Qt[t,]=A_operator(Q = Qt[(t-1),],transition_matrix = transition_matrix,time_interval = time_intervals[t],precision = 50L,methode=methode,A_abstol=abstol,A_reltol=reltol)
       if (methode!="sexpm"){Qt[t,]=mbd:::negatives_correction(Qt[t,],pars)}
       if (debug_check==1){print(head(Qt[t,]))}
-      
+
       #Selecting the state I am interested in
       vm = 1/choose((k+missnumspec),k)
       P  = vm * Qt[t,(missnumspec+1)] #I have to include +1 because of Q0
-      
+
       #Removing C and D effects from the LL
       loglik = log(P) + logB - sum(log(C)) - sum(log(D))
-      
+
       #Various checks
       loglik = as.numeric(loglik)
       if ( is.nan(loglik) | is.na(loglik) ){loglik=-Inf}
-      
+
       #CONDITIONING THE LIKELIHOOD ON THE SURVIVAL OF CROWN SPECIES
       Pc=1
       if ( (cond==1 | tips_interval[1]>1 | tips_interval[2]<Inf ) & !is.infinite(loglik) ){ #difference between sexpm and expo are not here
-        
+
         total_time=max(abs(brts));
         m=0:max_number_of_species
         one_over_Cm=(3*(m+1))/(m+3)
         one_over_qm_binom=1/choose((m+N0),N0)
         tips_components=(1+min_tips):(1+min(max_tips,max_number_of_species)) #applying tips constrain
-        
+
         Mk_N0=mbd:::create_A0(max_number_of_species = max_number_of_species,lambda = lambda,mu = mu,q = q,k = N0)
         A2_v1=A_operator(Q = Qt[1,],transition_matrix = Mk_N0,time_interval = total_time,precision = 50L,methode=methode,A_abstol=abstol,A_reltol=reltol)
         if (methode != "sexpm"){A2_v1=mbd:::negatives_correction(A2_v1,pars)} #it removes some small negative values that can occurr as bugs from the integration process
         if (debug_check == 1){print(head(A2_v1, max_tips))}
         total_product=A2_v1 * one_over_Cm * one_over_qm_binom
         Pc=sum(total_product[tips_components])
-        
+
         if (Pc==0){#slowest and best accuracy
           # ode_matrix=as.matrix(Mk_N0) #use this only if you use sparsematrices
           ode_matrix=MK_N0
@@ -140,12 +140,12 @@ mbd_loglik0 <- function(pars, brts, soc = 2, cond = 0, tips_interval = c(0,Inf),
           total_product=A2_v1*one_over_Cm*one_over_qm_binom
           Pc=sum(total_product[tips_components])
         }
-        
+
       }
       alpha = alpha + 5
     }
     loglik=loglik-log(Pc) #conditioned likelihood
-    
+
   }
   # loglik=-loglik #Rampal's optimizer uses loglik rather than -loglik
   return(loglik)
@@ -165,7 +165,7 @@ mbd_loglik_choosepar0 <- function(trparsopt, trparsfix, idparsopt = 1:3,
   #trparsopt are the values for parameters you want to analyze
   #idparsfix are the ids of the parameters you want to fix
   #trparsfix are the values for parameters you want to fix
-  
+
   namepars <- c("lambda","mu","q"); Npars <- length(namepars);
   # idparsopt=(1:3)[-c(idparsfix)] #this argument is useless but I let the user specify it because Rampal also did it (for some reason)
   trpars1 = rep(0,Npars)
@@ -251,9 +251,9 @@ mbd_ML0 <- function(brts, initparsopt, idparsopt, idparsfix = (1:3)[-idparsopt],
   # - maxiter = the maximum number of iterations in the optimization
   # - changeloglikifnoconv = if T the loglik will be set to -Inf if ML does not converge
   # - optimmethod = 'subplex' (current default) or 'simplex' (default of previous versions)
-  
+
   if (missing(parsfix) && (length(idparsfix)==0)){parsfix <- NULL}
-  
+
   options(warn=-1)
   namepars <- c("lambda","mu","q"); Npars <- length(namepars); #if you add more parameters to your model just change this
   failpars <- rep(-1,Npars); names(failpars) <- namepars; #those are the parameters that you get if something goes sideways
@@ -325,14 +325,14 @@ mbd_ML0 <- function(brts, initparsopt, idparsopt, idparsfix = (1:3)[-idparsopt],
           if(length(idparsfix) != 0) {MLpars1[idparsfix] <- parsfix}
           ML <- as.numeric(unlist(out$fvalues))
           out2 <- data.frame(t(MLpars1), loglik = ML, df = length(initparsopt), conv = unlist(out$conv))
-          
+
           tobeprint <- "Maximum likelihood parameter estimates:"
           for (ii in 1:Npars)
           {
             tobeprint <- paste(tobeprint,paste(names(MLpars1[ii]),":",sep = ""),MLpars1[ii])
           }
           s1 <- sprintf(tobeprint)
-          
+
           if(out2$conv != 0 & changeloglikifnoconv == T) { out2$loglik = -Inf }
           s2 = sprintf('Maximum loglikelihood: %f',ML)
           cat("\n",s1,"\n",s2,"\n\n")
@@ -344,8 +344,14 @@ mbd_ML0 <- function(brts, initparsopt, idparsopt, idparsfix = (1:3)[-idparsopt],
 }# bracket#1
 # mbd_ML_cluster0----------------
 #' @author Giovanni Laudanno
-#' @title Maximization of the loglikelihood under a multiple birth-death diversification model, wrapped for cluster usage.
-#' @description mbd_ML_cluster0 computes the maximum likelihood estimates of the parameters of a multiple birth-death diversification model. Differently from mbd_ML it needs only the number "s" of the simulations, making it suitable to run on a cluster. You will need two files to make it work: "general_settings","sim_data"; they are both generated by mbd_sim_dataset0.
+#' @title Maximization of the loglikelihood under a multiple birth-death
+#'   diversification model, wrapped for cluster usage.
+#' @description mbd_ML_cluster0 computes the maximum likelihood estimates
+#'   of the parameters of a multiple birth-death diversification model.
+#'   Differently from mbd_ML it needs only the number "s" of the simulations,
+#'   making it suitable to run on a cluster.
+#'   You will need two files to make it work: "general_settings",
+#'   "sim_data"; they are both generated by mbd_sim_dataset0.
 #' @param s The number of the simulation you want to evaluate.
 #' @param initparsopt The initial values of the parameters that must be optimized
 #' @param idparsopt The ids of the parameters that must be optimized. The ids are defined as follows:
@@ -373,18 +379,18 @@ mbd_ML_cluster0 <- function(s, initparsopt = c(1.8,0.3,0.15)){
   # initparsopt=c(1.8,0.3,0.15);
   parnames=c("lambda","mu","nu","q"); Npars = length(parnames)
   idparsopt=1:Npars;parsfix=NULL;
-  
+
   # simpath=paste("sims/",sim_pars[1],"-",sim_pars[2],"-",sim_pars[3],"/",sep = '')
   simpath = getwd()
-  
+
   datapath=paste(simpath,"/data",sep = '')
   load(file=paste(datapath,"/general_settings",sep = ''))
   load(file=paste(datapath,"/sim_data",sep = ''))
   print(s)
-  
+
   if ( !file.exists(paste(simpath,"/errors",sep = '')) ){dir.create(paste(simpath,"/errors",sep = ''))}
   sink(file = paste(simpath,"/errors/mbd_MLE_errors",s,".txt",sep = ''), append = T)
-  
+
   res <- mbd:::mbd_ML0(brts=sim_data[[s]],
                        initparsopt=initparsopt,
                        idparsopt=idparsopt,
@@ -399,7 +405,7 @@ mbd_ML_cluster0 <- function(s, initparsopt = c(1.8,0.3,0.15)){
                        maxiter = 1000 * round((1.25)^length(idparsopt)),
                        changeloglikifnoconv = FALSE,
                        optimmethod = 'subplex')
-  
+
   #additional tree info
   how_many_multiple=percent_multiple=-1;
   if (length(sim_data[[s]])>2){
@@ -411,7 +417,7 @@ mbd_ML_cluster0 <- function(s, initparsopt = c(1.8,0.3,0.15)){
   }
   # additional_species = sum( duplicated(sim_data[[s]]) );
   tips=length(sim_data[[s]])+1;
-  
+
   out=c(res[1:(Npars+1)],how_many_multiple,tips,percent_multiple,s);
   out2=out;#names(out2)=c("lambda","mu","q","LL","species born from multiple events","number of tips","percentage of species born from multiple events","tree id")
   names(out2)=c(parnames,"LL","species born from multiple events","number of tips","percentage of species born from multiple events","tree id")
@@ -426,7 +432,7 @@ mbd_ML_cluster0 <- function(s, initparsopt = c(1.8,0.3,0.15)){
   #out[8] = tree id
   sink()
   print(out2)
-  
+
   utils::write.table(matrix(out,ncol = length(out)),file = paste(simpath,"/mbd_MLE",s,".txt",sep = ''),append = T,row.names = F,col.names = F, sep = ",")
   if (res[1:4]!=c(-1,-1,-1,-1)){suppressWarnings(  file.remove( paste(simpath,"/errors/mbd_MLE_errors",s,".txt",sep = '') )  )}
 }
