f21a5b519198fe0cc63f3ccfa106a335f4daa789
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-15 09:25:36 +0200

    Removed 'hello' function

--- R/hello.R
+++
@@ -1,7 +0,0 @@
-#' Returns a string to say hello to the world
-#' @return a string to say hello to the world
-#' @author Hayley Wickham, modified by Richel J.C. Bilderbeek
-#' @export
-hello <- function() {
-  "Hello, world! Today is a good day!"
-}
