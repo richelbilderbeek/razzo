bc3213700f668de9b070ba0788669bef06eeccc7
Giappo <glaudanno@gmail.com>
2018-10-16 11:42:54 +0200

    bla

--- tests/testthat/test-raz_create_parameter_files.R
+++ tests/testthat/test-raz_create_parameter_files.R
@@ -1,6 +1,10 @@
 context("raz_create_parameters_files")
 
 test_that("use", {
-  filenames <- raz_create_parameters_files()
+  # Will be something like 'home/tece/razzo_project/data
+  razzo_project_data_folder <- file.path(rappdirs::user_data_dir(), "razzo_project", "data")
+  filenames <- raz_create_parameters_files(folder = razzo_project_data_folder)
   expect_true(length(filenames) >= 1)
+  # TODO: add test that foldername is in filename, e.g. 'myfolder/1.csv'
+  expect_true(all(file.exists(filenames)))
 })
