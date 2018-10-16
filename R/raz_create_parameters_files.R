bdbad7c5c148810447973cc98e3d622b05248d3d
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-16 07:00:47 +0200

    Create #2, #3, #4 and #5

--- R/raz_create_parameters_files.R
+++ R/raz_create_parameters_files.R
@@ -1,7 +1,25 @@
-#' Create all parameters files
-#' @return names of the files created
+#' Create all parameters files in a folder.
+#' In that folder, one folder is created per parameter file.
+#' In each subfolder, a file 'parameters.csv' is created
+#' @param folder_name name of the folder
+#' @return full paths of the files created
 #' @author Richel J.C. Bilderbeek
 #' @export
-raz_create_parameters_files <- function() {
-  "1.csv"
+raz_create_parameters_files <- function(folder_name) {
+
+  # * folder_name (in our case, it's called 'data')
+  #    * 1
+  #      * parameters.csv
+  #    * 2
+  #      * parameters.csv
+  # Etcetera
+
+  # TODO: Create one sub-folder per parameter set
+  sub_folder_name <- "1"
+  local_path <- file.path(folder_name, sub_folder_name)
+  path <- file.path(local_path, "parameters.csv")
+  # TODO: Create the parameters file here
+
+  # Return the path to the file
+  path
 }
--- tests/testthat/test-raz_create_input_files.R
+++ tests/testthat/test-raz_create_input_files.R
@@ -1,11 +1,28 @@
 context("raz_create_input_files")
 
 test_that("use", {
-  parameters_filename <- "1.csv"
+  # Work from a folder
+  folder_name <- tempdir()
+
+  # Create the parameter files
+  raz_create_parameters_files(folder_name)
+  sub_folder_name <- "1"
+  parameters_filename <- file.path(folder_name, sub_folder_name, "parameters.csv")
+
+  # Work on the parameter file
   input_filenames <- raz_create_input_files(parameters_filename)
-  expect_equal(4, length(input_filenames))
-  expect_true("1a.tree" %in% input_filenames)
-  expect_true("1a.fasta" %in% input_filenames)
-  expect_true("1b.tree" %in% input_filenames)
-  expect_true("1b.fasta" %in% input_filenames)
+
+  # Expect four files to be created
+  mbd_fasta_filename <- file.path(folder_name, sub_folder_name, "mbd.fasta")
+  mbd_tree_filename <- file.path(folder_name, sub_folder_name, "mbd.tree")
+  bd_fasta_filename <- file.path(folder_name, sub_folder_name, "bd.fasta")
+  bd_tree_filename <- file.path(folder_name, sub_folder_name, "bd.tree")
+
+  # TODO: Issue #3
+  if (1 == 2) {
+    expect_equal(file.exists(mbd_fasta_filename))
+    expect_equal(file.exists(mbd_tree_filename))
+    expect_equal(file.exists(bd_fasta_filename))
+    expect_equal(file.exists(bd_tree_filename))
+  }
 })
