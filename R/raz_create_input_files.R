5bf396c409c3e4e9ade80de1ccfae79a38f6aaa2
Giappo <glaudanno@gmail.com>
2018-10-16 12:25:09 +0200

    raz_create_input_files added and tested

--- R/raz_create_input_files.R
+++ R/raz_create_input_files.R
@@ -13,14 +13,10 @@
 #' @export
 raz_create_input_files <- function(parameters_filename) {
 
-  # TODO: check for parameter file and use its parameters
-  if (1 == 2) {
-    testit::assert(file.exists(parameters_filename))
-  }
+  testit::assert(file.exists(parameters_filename))
 
   # Read the parameters
   parameters <- raz_open_parameters_file(parameters_filename)
-  testit::assert(parameters$lambda >= 0.0)
 
   # Get the four filenames
   folder_name <- dirname(parameters_filename)
@@ -29,12 +25,9 @@ raz_create_input_files <- function(parameters_filename) {
   bd_tree_filename <- file.path(folder_name, "bd.tree")
   bd_alignment_filename <- file.path(folder_name, "bd.fasta")
 
-  # TODO: create the four files
-  if (1 == 2) {
-    # Create an MBD tree
-    raz_create_mbd_tree(parameters, mbd_tree_filename)
-    testit::assert(file.exists(mbd_tree_filename))
-  }
+  # Create an MBD tree
+  raz_create_mbd_tree(parameters, mbd_tree_filename)
+  testit::assert(file.exists(mbd_tree_filename))
 
   # Return the filenames
   input_filenames <- c(
--- tests/testthat/test-raz_create_input_files.R
+++ tests/testthat/test-raz_create_input_files.R
@@ -5,24 +5,47 @@ test_that("use", {
   folder_name <- tempdir()
 
   # Create the parameter files
-  raz_create_parameters_files(folder_name)
-  sub_folder_name <- "1"
-  parameters_filename <- file.path(folder_name, sub_folder_name, "parameters.csv")
+  soc <- 2
+  age <- 10
+  cond <- 1
+  filenames <- raz_create_parameters_files(folder_name = folder_name,
+                                           lambda.interval = c(0.2, 0.2),
+                                           mu.interval = c(0.15, 0.15),
+                                           nu.interval = seq(from = 1, to = 2.5, by = 0.5),
+                                           q.interval = seq(from = 0.10, to = 0.20, by = 0.05),
+                                           seed.interval = 1:3,
+                                           soc = soc,
+                                           age = age,
+                                           cond = cond)
+  # sub_folder_name <- "1"
+  # parameters_filename <- file.path(folder_name, sub_folder_name, "parameters.csv")
+
+  parameters_filename <- filenames[1]
 
   # Work on the parameter file
   input_filenames <- raz_create_input_files(parameters_filename)
+  testthat::expect_true(
+    length(unique(dirname(input_filenames))) == 1
+  )
+  directory_name <- unique(dirname(input_filenames))
 
   # Expect four files to be created
-  mbd_fasta_filename <- file.path(folder_name, sub_folder_name, "mbd.fasta")
-  mbd_tree_filename <- file.path(folder_name, sub_folder_name, "mbd.tree")
-  bd_fasta_filename <- file.path(folder_name, sub_folder_name, "bd.fasta")
-  bd_tree_filename <- file.path(folder_name, sub_folder_name, "bd.tree")
+  # mbd_fasta_filename <- file.path(folder_name, sub_folder_name, "mbd.fasta")
+  # mbd_tree_filename <- file.path(folder_name, sub_folder_name, "mbd.tree")
+  # bd_fasta_filename <- file.path(folder_name, sub_folder_name, "bd.fasta")
+  # bd_tree_filename <- file.path(folder_name, sub_folder_name, "bd.tree")
 
-  # TODO: Issue #3
+  mbd_fasta_filename <- file.path(directory_name, "mbd.fasta")
+  mbd_tree_filename  <- file.path(directory_name, "mbd.tree")
+  bd_fasta_filename  <- file.path(directory_name, "bd.fasta")
+  bd_tree_filename   <- file.path(directory_name, "bd.tree")
+
+  testthat::expect_true(file.exists(mbd_tree_filename))
+  # these files are not created by raz_create_input_files
   if (1 == 2) {
-    expect_equal(file.exists(mbd_fasta_filename))
-    expect_equal(file.exists(mbd_tree_filename))
-    expect_equal(file.exists(bd_fasta_filename))
-    expect_equal(file.exists(bd_tree_filename))
+    testthat::expect_true(file.exists(mbd_fasta_filename))
+    testthat::expect_true(file.exists(bd_tree_filename))
+    testthat::expect_true(file.exists(bd_fasta_filename))
   }
+
 })
