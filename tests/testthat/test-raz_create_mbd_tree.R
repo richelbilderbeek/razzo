75038601f5eb0b88efd8e4eede98a2fb879a039c
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-18 08:54:33 +0200

    Add #24

--- tests/testthat/test-raz_create_mbd_tree.R
+++ tests/testthat/test-raz_create_mbd_tree.R
@@ -37,3 +37,43 @@ test_that("use", {
   testthat::expect_true(file.exists(mbd_tree_filename))
 
 })
+
+test_that("must have as much multiple bursts as predicted by nu", {
+
+  # Work from a folder
+  folder_name <- razzo:::raz_tempdir(); # folder_name <- tempdir()
+  testthat::expect_true(
+    dir.exists(folder_name)
+  )
+
+  # Create the parameter files
+  filenames <- razzo::raz_create_parameters_files(folder_name = folder_name)
+  testthat::expect_true(length(filenames) > 0)
+  one_parameter_setting <- dirname(filenames[1])
+  testthat::expect_true(file.exists(one_parameter_setting))
+  parameters_filename <- file.path(one_parameter_setting, "parameters.csv")
+  mbd_tree_filename   <- file.path(one_parameter_setting, "mbd.tree")
+
+  # Get parameters
+  parameters <- razzo::raz_open_parameters_file(parameters_filename)
+
+  # Create tree
+  razzo::raz_create_mbd_tree(parameters = parameters, folder_name = folder_name)
+  testit::assert(file.exists(mbd_tree_filename))
+
+  skip("TODO: Issue #24: create trees with the expected number of events")
+  mbd_tree <- ape::read.tree(file = mbd_tree_filename)
+
+  # Calculate the number of expected triggered speciation events
+  exp_n_spec_events <- mbd::mbd_calc_exp_n_spec_events(
+    nu = parameters$nu,
+    time <- parameters$crown_age
+  )
+
+  # Count the number of actual triggered speciation events
+  n_spec_events <- mbd::mbd_count_n_spec_events(mbd_tree)
+
+  expect_equal(exp_n_spec_events, n_spec_events)
+
+
+})
