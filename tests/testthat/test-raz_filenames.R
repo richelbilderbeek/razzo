22549fb7c4ea0e19b429bfd277e37a716b8457e8
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-22 16:48:19 +0200

    Lines should be less than 80 characters, thanks @lintr-bot

--- tests/testthat/test-raz_filenames.R
+++ tests/testthat/test-raz_filenames.R
@@ -13,19 +13,26 @@ test_that("use", {
   filenames <- razzo:::raz_get_standard_test_filenames()
 
   parameters_filename <- filenames[1]
-  parameters <- razzo::raz_open_parameters_file(parameters_filename = parameters_filename)
+  parameters <- razzo::raz_open_parameters_file(
+    parameters_filename = parameters_filename)
 
   # Get filenames
-  mbd_tree_filename  <- razzo::raz_create_filename_mbd_tree(parameters = parameters, folder_name = folder_name)
-  bd_tree_filename   <- razzo::raz_create_filename_bd_tree(parameters = parameters, folder_name = folder_name)
-  mbd_fasta_filename <- razzo::raz_create_filename_mbd_alignment(parameters = parameters, folder_name = folder_name)
-  bd_fasta_filename  <- razzo::raz_create_filename_bd_alignment(parameters = parameters, folder_name = folder_name)
+  mbd_tree_filename  <- razzo::raz_create_filename_mbd_tree(
+    parameters = parameters, folder_name = folder_name)
+  bd_tree_filename   <- razzo::raz_create_filename_bd_tree(
+    parameters = parameters, folder_name = folder_name)
+  mbd_fasta_filename <- razzo::raz_create_filename_mbd_alignment(
+    parameters = parameters, folder_name = folder_name)
+  bd_fasta_filename  <- razzo::raz_create_filename_bd_alignment(
+    parameters = parameters, folder_name = folder_name)
   filenames <- c(mbd_fasta_filename,
                  mbd_tree_filename,
                  bd_fasta_filename,
                  bd_tree_filename)
 
-  parameters_path <- razzo::raz_get_parameters_path(parameters = parameters, folder_name = folder_name)
+  parameters_path <- razzo::raz_get_parameters_path(
+    parameters = parameters, folder_name = folder_name
+  )
 
   # Expect four files to be created
   exp_mbd_fasta_filename <- file.path(parameters_path, "mbd.fasta")
