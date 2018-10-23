644eb8818360b81cdfe4e3f611d6026b0537c3a2
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-23 07:48:18 +0200

    Simple test for 'raz_open_parameters_file'. Fix #7

--- tests/testthat/test-raz_open_parameters_file.R
+++ tests/testthat/test-raz_open_parameters_file.R
@@ -2,20 +2,7 @@ context("raz_open_parameters_file")
 
 test_that("use", {
 
-  # Work from a folder
-  folder_name <- razzo:::raz_make_tempdir()
-  testthat::expect_true(
-    dir.exists(folder_name)
-  )
-
-  skip("TODO: fix test")
-
-  # Create the parameter files
-  razzo:::raz_save_test_params()
-  filenames <- razzo:::raz_get_test_filenames()
-
-  filename <- filenames[1]
-  parameters <- razzo::raz_open_parameters_file(filename)
+  parameters <- razzo::raz_open_parameters_file(raz_get_path("parameters.csv"))
   testthat::expect_true(parameters$lambda > 0.0)
   testthat::expect_true(parameters$mu >= 0.0)
   testthat::expect_true(parameters$nu >= 0.0)
