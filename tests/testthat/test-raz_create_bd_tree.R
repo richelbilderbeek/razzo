555f6bbae5ac166a108d562cdbc3b74a38102063
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-24 14:52:38 +0200

    Add 'test-raz_create_bd_*' back in, for #39

---
+++ tests/testthat/test-raz_create_bd_tree.R
@@ -0,0 +1,18 @@
+context("raz_create_bd_tree")
+
+test_that("use", {
+
+  parameters <- raz_open_parameters_file(raz_get_path("parameters.csv"))
+  mbd_tree <- ape::read.tree(file = raz_get_path("mbd.tree"))
+  mbd_l_matrix <- as.matrix(
+    utils::read.csv(file = raz_get_path("mbd_l_matrix.csv")))
+  # Remove the first column?
+  mbd_l_matrix <- mbd_l_matrix[, -1]
+
+  bd_tree <- raz_create_bd_tree(
+    parameters = parameters,
+    mbd_tree = mbd_tree,
+    mbd_l_matrix = mbd_l_matrix
+  )
+  expect_equal(class(bd_tree), "phylo")
+})
---
+++ tests/testthat/test-raz_create_bd_tree_file.R
@@ -0,0 +1,18 @@
+context("raz_create_bd_tree_file")
+
+test_that("use", {
+
+  parameters_filename <- raz_create_tempfile("parameters.csv")
+  raz_create_tempfile("mbd.tree")
+  raz_create_tempfile("mbd_l_matrix.csv")
+
+  bd_tree_filename <- raz_create_bd_tree_file(
+    parameters_filename = parameters_filename
+  )
+  expect_true(file.exists(bd_tree_filename))
+  expect_true(length(grep(
+    pattern = "bd\\.tree$", bd_tree_filename, perl = TRUE, value = TRUE))
+    > 0
+  )
+
+})
