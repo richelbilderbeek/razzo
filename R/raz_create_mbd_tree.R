554b996a5c1fdeada62136260f7eab2d5d21a36c
Giappo <glaudanno@gmail.com>
2018-11-05 13:56:40 +0100

    changed variable N0 (or soc) to n_0

--- R/raz_create_mbd_tree.R
+++ R/raz_create_mbd_tree.R
@@ -12,16 +12,16 @@ raz_create_mbd_tree <- function(
   parameters
 ) {
   mbd_pars <- c(parameters$lambda, parameters$mu, parameters$nu, parameters$q)
-  soc <- 2 # Use crown age
+  n_0 <- 2 # two species at time zero
   age <- parameters$crown_age
   cond <- 1 # Condition on non-extinction
   seed <- parameters$seed
   testit::assert(!is.null(seed))
   testit::assert(!is.null(age))
-  testit::assert(!is.null(soc))
+  testit::assert(!is.null(n_0))
   testit::assert(!is.null(cond))
 
   set.seed(seed)
-  mbd_sim <- mbd::mbd_sim(pars = mbd_pars, soc = soc, age = age, cond = cond)
-  list(mbd_tree = mbd_sim$tes, mbd_l_matrix = mbd_sim$l_matrix)
+  mbd_sim <- mbd::mbd_sim(pars = mbd_pars, n_0 = n_0, age = age, cond = cond)
+  list(mbd_tree = mbd_sim$reconstructed_tree, mbd_l_matrix = mbd_sim$l_matrix)
 }
