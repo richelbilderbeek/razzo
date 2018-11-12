99fbf56477774d1ef0dce46b8ec04b92f31db6ff
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-12 11:30:09 +0100

    Add test for #77

---
+++ tests/testthat/test-raz_collect_mar_liks.R
@@ -0,0 +1,24 @@
+context("test-raz_collect_mar_liks")
+
+test_that("use", {
+  skip("TODO. Issue 77, #77")
+
+  df <- raz_collect_mar_log_liks(raz_get_path("razzo_project"))
+
+  # Experimental parameters that vary
+  expect_true("lambda" %in% names(parameters))
+  expect_true("mu" %in% names(parameters))
+  expect_true("nu" %in% names(parameters))
+  expect_true("q" %in% names(parameters))
+  expect_true("seed" %in% names(parameters))
+  expect_true("site_model" %in% names(parameters))
+  expect_true("clock_model" %in% names(parameters))
+
+  # The collected marginal log-likelihood
+  expect_true("mar_log_lik" %in% names(parameters))
+
+  # Data must be tidy
+  expect_true(is.factor(df$clock_model))
+  expect_true(is.factor(df$site_model))
+
+})
