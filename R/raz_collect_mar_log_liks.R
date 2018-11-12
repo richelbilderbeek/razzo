0ede0efe4b89c6f146464b85067465fafd42f8c9
Giappo <glaudanno@gmail.com>
2018-11-12 13:57:25 +0100

    raz_get_path now imports files correctly on my local machine

---
+++ R/raz_collect_mar_log_liks.R
@@ -0,0 +1,11 @@
+#' @title Collect marginal loglikelihoods
+#' @description Collect marginal loglikelihoods
+#' @inheritParams default_params_doc
+#' @return nothing
+#' @author Giovanni Laudanno
+#' @export
+raz_collect_mar_log_liks <- function(
+
+) {
+
+  }
