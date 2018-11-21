b6920b1b2b14f3678bedb49d3731bcea4487ea00
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-21 07:15:52 +0100

    Remove obsolete test

--- tests/testthat/test-raz_create_mbd_tree.R
+++ tests/testthat/test-raz_create_mbd_tree.R
@@ -17,24 +17,3 @@ test_that("creates a tree", {
   # Should preferably be a data.frame or tibble
   expect_equal(class(mbd_sim$mbd_l_matrix), "matrix")
 })
-
-test_that("must have as much multiple bursts as predicted by nu", {
-
-  skip(
-    "see 'context('raz_create_mbd_tree_events')'"
-  )
-
-  parameters <- razzo::raz_open_parameters_file(raz_get_path("parameters.csv"))
-
-  mbd_sim <- razzo::raz_create_mbd_tree(parameters)
-  tree <- mbd_sim$mbd_tree
-
-  # Calculate the number of expected triggered speciation events
-  exp_n_spec_events <- parameters$crown_age / parameters$nu
-
-
-  # Count the number of actual triggered speciation events
-  n_spec_events <- mbd::mbd_count_n_spec_events(mbd_tree)
-
-  expect_equal(exp_n_spec_events, n_spec_events)
-})
