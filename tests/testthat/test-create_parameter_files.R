57e0ae3c47f847f936b0ea74817b28bdb51e81c2
richelbilderbeek <richel@richelbilderbeek.nl>
2018-11-27 13:57:02 +0100

    Do minimal test of parameter creation for full experiment

--- tests/testthat/test-create_parameter_files.R
+++ tests/testthat/test-create_parameter_files.R
@@ -40,6 +40,46 @@ test_that("use", {
 
 })
 
+test_that("use, full", {
+
+  # Put files in temporary folder
+  super_folder_name <- tempdir()
+  project_folder_name <- file.path(super_folder_name, "razzo_project")
+  dir.create(path = project_folder_name)
+
+  filenames <- create_parameters_files(
+    project_folder_name = project_folder_name,
+    experiment_type = "full"
+  )
+
+  # The folder structure created:
+  # * razzo_project (the name of the GitHub containing the scripts)
+  #   * scripts
+  #   * data
+  #     * folder named after parameters, e.g. '0.2-0.15-2.5-0.1'
+  #       * folder named after seed, e.g. '1'
+  #   * figures
+
+  # OK: Parameter filenames end with 'parameters.csv'
+  testthat::expect_true(
+    length(
+      grep(
+        pattern = "parameters\\.csv$", filenames[1], perl = TRUE, value = TRUE
+      )
+    ) > 0
+  )
+  # OK: there is a data folder that is a subfolder of razzo_project'
+  # Use ..? to indicate one or two back- or normal slashes
+  testthat::expect_true(
+    length(
+      grep(
+        pattern = "razzo_project..?data",
+        filenames[1], perl = TRUE, value = TRUE
+      )
+    ) > 0
+  )
+
+})
 test_that("can read", {
 
   # Create parameter files from fresh
