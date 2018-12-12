f9c98d77b5cdebb9997ce989f6c4e402deb48cad
richelbilderbeek <richel@richelbilderbeek.nl>
2018-12-12 17:24:20 +0100

    Add stubs for 'predict_n_extinct/extant_species'

---
+++ tests/testthat/test-predict_n_extinct_taxa.R
@@ -0,0 +1,25 @@
+context("test-predict_n_extinct_taxa")
+
+test_that("use", {
+
+  wkip("WIP")
+  lambda <- 0.5
+  mu <- 0.2
+  nu <- 0.5
+  q <- 0.4
+  crown_age <- 15.0
+
+  n_baseline <- predict_n_extinct_taxa(pars = c(lambda, mu, nu, q), crown_age = crown_age)
+
+  # Expect more extinct taxa for higher speciation rate
+  testit::assert(baseline < predict_n_extinct_taxa(pars = c(lambda * 2.0, mu, nu, q), crown_age = crown_age))
+
+  # Expect more extinct taxa for lower extinction rate
+  testit::assert(baseline < predict_n_extinct_taxa(pars = c(lambda, mu * 2.0, nu, q), crown_age = crown_age))
+
+  # Expect more extinct taxa for higher number of MBD events
+  testit::assert(baseline < predict_n_extinct_taxa(pars = c(lambda, mu, nu * 2.0, q), crown_age = crown_age))
+
+  # Expect more extinct taxa for more intense MBD events
+  testit::assert(baseline < predict_n_extinct_taxa(pars = c(lambda, mu, nu, q * 2.0), crown_age = crown_age))
+})
