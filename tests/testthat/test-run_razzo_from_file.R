fa6fb829545d63963bd3b5cc0b0340241ad44f80
richelbilderbeek <richel@richelbilderbeek.nl>
2019-02-14 11:45:40 +0100

    'run_razzo_from_file' works locally

--- tests/testthat/test-run_razzo_from_file.R
+++ tests/testthat/test-run_razzo_from_file.R
@@ -3,26 +3,20 @@ context("test-run_razzo_from_file")
 test_that("use", {
 
   if (!beastier::is_on_travis()) return()
-  skip("Not like this")
+
   super_folder_name <- tempdir()
   project_folder_name <- file.path(super_folder_name, "razzo_project")
   dir.create(path = project_folder_name, recursive = TRUE, showWarnings = FALSE)
-  parameters_filenames <- create_parameters_files(
-    project_folder_name = project_folder_name
-  )
-  parameters_filename <- parameters_filenames[1]
-  expect_silent(
-    razzo::run_razzo_from_file(
-      parameters_filename = parameters_filename
-    )
-  )
+  parameters_filenames <- create_test_parameters_files(project_folder_name)
+  # Only run the first
+  expect_silent(run_razzo_from_file(parameters_filenames[1]))
 })
 
 test_that("abuse", {
 
   parameters_filename <- "neverland"
   expect_error(
-    razzo::run_razzo_from_file(
+    run_razzo_from_file(
       parameters_filename = parameters_filename
     )
   )
