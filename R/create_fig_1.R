3d3d5caa669b9b111f5722502121555994573b63
Giappo <glaudanno@gmail.com>
2019-03-01 18:42:12 +0100

    bla

--- R/create_fig_1.R
+++ R/create_fig_1.R
@@ -9,49 +9,77 @@ create_fig_1 <- function(
 ) {
   check_project_folder_name(project_folder_name)
 
-  # ISSUE #138 STUB
-
-  lambda <- 0.2
-  mu <- 0.1
-  nu <- 2
-  q <- 0.1
-
-  df <- data.frame(
-   par_setting = c(1, 2),
-   nltt = c(0.1, 0.2),
-   gen_model = c("1", "2")
+  df0 <- collect_nltt_stats(project_folder_name)
+  df0$par_setting <- interaction(
+    df0$lambda,
+    df0$mu,
+    df0$nu,
+    df0$q,
+    df0$site_model,
+    df0$clock_model,
+    df0$tree_prior
   )
-  if (1) {
-    # need to call these variables to avoid notes in check
-    df2 <- df
-    df2$nltt <- df$nltt
-    df2$par_setting <- df$par_setting
-    df2$gen_model <- df$gen_model
-    par_setting <- nltt <- gen_model <- 0
-    lambda + mu + q + nu +
-      length(df2$par_setting) + length(df2$nltt)  + length(df2$gen_model) > 0
-  }
-  xlabels <- c("1", "2")
-  pl <- ggplot2::ggplot(df) +
-    ggplot2::geom_boxplot(ggplot2::aes(
-      x = par_setting,
-      y = nltt,
-      color = gen_model
-    )) +
-    ggplot2::facet_grid(. ~ gen_model
-    ) +
-    ggplot2::xlab(bquote(
-      "Parameter setting" ~ "(" ~ lambda ~ "," ~ mu ~ ","
-      ~ nu ~ "," ~ q ~ ")"
-    )) +
-    ggplot2::ylab(bquote(
-      nltt
-    )) +
-    ggplot2::theme(
-      axis.line = ggplot2::element_line(
-        colour = "darkblue",
-        size = 1,
-        linetype = "solid"
+  experiments <- c("gen", "best")
+  real_names <- c("generative", "best candidate")
+  pl <- vector("list", length(experiments))
+  for (i in seq_along(experiments)) {
+    df00 <- df0[df0$best_or_gen == experiments[i], ]
+    df <- tidyr::gather(
+      df00,
+      "i",
+      "nltt",
+      (1:ncol(df0))[grepl("nltt", names(df0))]
+    )
+    if (1) {
+      # need to call these variables to avoid notes in check
+      df2 <- df
+      df2$nltt <- df$nltt
+      df2$par_setting <- df$par_setting
+      df2$gen_model <- df$gen_model
+      par_setting <- nltt <- gen_model <- 0
+      lambda + mu + q + nu +
+        length(df2$par_setting) + length(df2$nltt)  + length(df2$gen_model) > 0
+    }
+    xlabels <- unique(paste0(
+      df0$lambda,
+      "\n",
+      df$mu,
+      "\n",
+      df$nu,
+      "\n",
+      df$q,
+      "\n",
+      df$site_model,
+      "\n",
+      df$clock_model,
+      "\n",
+      df$tree_prior
+    ))
+    pl[[i]] <- ggplot2::ggplot(df) +
+      ggplot2::geom_boxplot(ggplot2::aes(
+        x = par_setting,
+        y = nltt,
+        color = gen_model
       )) +
-    ggplot2::scale_x_discrete(labels = xlabels); pl
+      ggplot2::facet_grid(. ~ gen_model
+      ) +
+      ggplot2::xlab(bquote(
+        "Parameter setting" ~ "(" ~ lambda ~ "," ~ mu ~ ","
+        ~ nu ~ "," ~ q ~ "," ~ site ~ "," ~ clock ~ "," ~ prior ~ ")"
+      )) +
+      ggplot2::ylab(bquote(
+        nltt
+      )) +
+      ggplot2::theme(
+        axis.line = ggplot2::element_line(
+          colour = "darkblue",
+          size = 1,
+          linetype = "solid"
+        )) +
+      ggplot2::scale_x_discrete(
+        labels = xlabels
+        ) +
+      ggplot2::ggtitle(real_names[i])
+  }
+  gridExtra::grid.arrange(pl[[1]], pl[[2]], nrow = 1)
 }
