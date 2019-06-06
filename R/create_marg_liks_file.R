d020b1dca3a7174ca575d645c43392b4f340b83a
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-06 13:21:44 +0200

    Reovered 'create_marg_liks_file

---
+++ R/create_marg_liks_file.R
@@ -0,0 +1,27 @@
+#' @title Creates file containing marginal loglikelihoods
+#' @description Creates file containing marginal loglikelihoods
+#' @inheritParams default_params_doc
+#' @return the name of the saved file
+#' @author Giovanni Laudanno
+#' @export
+create_marg_liks_file <- function(
+  project_folder_name = getwd()
+) {
+
+  # create the dataframe
+  marg_liks <- collect_marg_liks(project_folder_name) # nolint internal function
+
+  # save output
+  results_folder <- get_results_path(project_folder_name) # nolint
+  # No warning if folder already exists
+  dir.create(results_folder, showWarnings = FALSE)
+
+  marg_liks_filename <- file.path(
+    results_folder,
+    "marg_liks.csv"
+  )
+  utils::write.csv(x = marg_liks, file = marg_liks_filename)
+
+  # return file name
+  marg_liks_filename
+}
