d10aaed03bb0d8e1c263ddf2375d6d5d60113821
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-17 11:22:04 +0200

    Fix doc

--- R/is_pff.R
+++ R/is_pff.R
@@ -1,4 +1,5 @@
 #' Is the filename a Peregrine-friendly filename?
+#' @param filename name of the file
 #' @export
 is_pff <- function(filename) {
   all(is.na(stringr::str_match(string = filename, pattern = "(/local)?/tmp/")))
