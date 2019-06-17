ce9be8ce15537ba352db91b83050e15aec74ecec
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-17 10:56:18 +0200

    Use correct scripts folder

---
+++ scripts/install_r_packages.R
@@ -0,0 +1,6 @@
+# Script to install razzo and its dependencies
+devtools::install_github("ropensci/mauricer")
+devtools::install_github("ropensci/babette"
+devtools::install_github("richelbilderbeek/mcbette")
+devtools::install_github("richelbilderbeek/pirouette")
+devtools::install_github("richelbilderbeek/razzo", ref = "richel")
