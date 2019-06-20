6bf344084b9e9c8eec938a57afbb8c2043fe5166
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-20 09:52:55 +0200

    Full parameter set uses MCMC with length 10K

--- R/create_files_razzo_paramses.R
+++ R/create_files_razzo_paramses.R
@@ -48,7 +48,8 @@ create_parameters_files <- create_files_razzo_paramses <- function(
     )
     parameters_filenames <- save_razzo_paramses(
       project_folder_name = project_folder_name,
-      mbd_paramses = mbd_paramses
+      mbd_paramses = mbd_paramses,
+      mcmc_chain_length = 10000
     )
     testit::assert(nrow(mbd_paramses) == length(parameters_filenames))
   }
