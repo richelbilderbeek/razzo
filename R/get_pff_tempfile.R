5dfa0b0e5d67ef250ae585aa9e6bce20d9c7e9f9
richelbilderbeek <richel@richelbilderbeek.nl>
2019-06-25 11:26:02 +0200

    Improve doc for Peregrine-friendly functions

--- R/get_pff_tempfile.R
+++ R/get_pff_tempfile.R
@@ -5,6 +5,13 @@
 #' @param fileext see \link{tempfile}
 #' @seealso use \link{get_pff_tempdir}
 #' to get a Peregrine-friendly folder name
+#' @examples
+#' library(testthat)
+#'
+#' expect_true(is_pff(get_pff_tempdir()))
+#' expect_true(is_pff(get_pff_tempfile()))
+#' expect_false(is_pff("/local/tmp/peregrine_unfriendly"))
+#' expect_false(is_pff("/tmp/peregrine_unfriendly"))
 #' @author Richel J.C. Bilderbeek
 #' @export
 get_pff_tempfile <- function(
--- R/is_pff.R
+++ R/is_pff.R
@@ -2,6 +2,13 @@
 #' @param filename name of the file
 #' @seealso see \link{get_pff_tempdir} and \link{get_pff_tempfile}
 #' to get a Peregrine-friendly temporary folder and filename
+#' @examples
+#' library(testthat)
+#'
+#' expect_true(is_pff(get_pff_tempdir()))
+#' expect_true(is_pff(get_pff_tempfile()))
+#' expect_false(is_pff("/local/tmp/peregrine_unfriendly"))
+#' expect_false(is_pff("/tmp/peregrine_unfriendly"))
 #' @author Richel J.C. Bilderbeek
 #' @export
 is_pff <- function(filename) {
