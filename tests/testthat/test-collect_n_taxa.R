879067fb6603bb2f73205fb5ceaf0288702c4a54
richelbilderbeek <richel@richelbilderbeek.nl>
2019-07-01 13:17:51 +0200

    Expose #210

---
+++ tests/testthat/test-collect_n_taxa.R
@@ -0,0 +1,32 @@
+test_that("use", {
+
+  skip("Issue #210")
+  if (1 == 2) {
+    # Run the experiment if you can and need to
+    for (file in list.files(get_razzo_path("razzo_project"), recursive = TRUE, pattern = "parameters\\.RDa")) {
+      run_razzo(open_parameters_file(file))
+    }
+  }
+  df <- collect_n_taxa(
+    project_folder_name = get_razzo_path("razzo_project")
+  )
+
+  # Experimental parameters that vary
+  expect_true("lambda" %in% names(df))
+  expect_true("mu" %in% names(df))
+  expect_true("nu" %in% names(df))
+  expect_true("q" %in% names(df))
+  expect_true("seed" %in% names(df))
+  expect_true("site_model" %in% names(df))
+  expect_true("clock_model" %in% names(df))
+  expect_true("tree" %in% names(df))
+  # The number of taxa
+  expect_true("n_taxa" %in% names(df))
+
+  # Data must be tidy
+  expect_true(is.factor(df$clock_model))
+  expect_true(is.factor(df$site_model))
+
+  # Rows must be unique
+  expect_equal(nrow(unique(df)), nrow(df))
+})
