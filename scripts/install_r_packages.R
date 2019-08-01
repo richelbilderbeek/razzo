68aafae5e179a21ab42786ffceeb635f33cb83a8
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-01 11:04:00 +0200

    Install all deps [skip ci]

--- scripts/install_r_packages.R
+++ scripts/install_r_packages.R
@@ -1,6 +1,10 @@
 # Script to install razzo and its dependencies
+devtools::install_github("ropensci/beautier")
+devtools::install_github("ropensci/tracerer")
+devtools::install_github("ropensci/beastier")
 devtools::install_github("ropensci/mauricer")
 devtools::install_github("ropensci/babette")
 devtools::install_github("richelbilderbeek/mcbette")
-devtools::install_github("richelbilderbeek/pirouette")
+devtools::install_github("richelbilderbeek/pirouette", ref = "richel")
+devtools::install_github("richelbilderbeek/peregrine")
 devtools::install_github("richelbilderbeek/razzo", ref = "richel")
