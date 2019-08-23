eb66738bf37802d00f9d269d452f69872e80f4f6
richelbilderbeek <richel@richelbilderbeek.nl>
2019-08-23 13:03:21 +0200

    Use '2e3' instead of '2 * 10^3' notation, thanks @lintr-bot

--- R/create_razzo_nested_sampling_mcmc.R
+++ R/create_razzo_nested_sampling_mcmc.R
@@ -1,11 +1,11 @@
 #' Create a nested sampling MCMC that matches the article
 #' @export
 create_razzo_nested_sampling_mcmc <- function() {
-  est_evidence_mcmc = beautier::create_nested_sampling_mcmc(
-    chain_length = 10^6,
-    store_every = 10^3,
+  beautier::create_nested_sampling_mcmc(
+    chain_length = 1e6,
+    store_every = 1e3,
     particle_count = 1,
-    sub_chain_length = 5.0*10^3,
-    epsilon = 10^-12
+    sub_chain_length = 5e3,
+    epsilon = 1e-12
   )
 }
