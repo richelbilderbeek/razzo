248f78d561728c2cfe1361b36b485f94f8fe70af
richelbilderbeek <richel@richelbilderbeek.nl>
2019-09-06 14:03:16 +0200

    Use 10 NS particles

--- R/create_razzo_nested_sampling_mcmc.R
+++ R/create_razzo_nested_sampling_mcmc.R
@@ -4,7 +4,7 @@ create_razzo_nested_sampling_mcmc <- function() { # nolint indeed a long functio
   beautier::create_nested_sampling_mcmc(
     chain_length = 1e6,
     store_every = 1e3,
-    particle_count = 1,
+    particle_count = 10,
     sub_chain_length = 5e3,
     epsilon = 1e-12
   )
