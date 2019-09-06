27f75b637d3b24eb9b5f87b3ae725fe44abcf5e0
richelbilderbeek <richel@richelbilderbeek.nl>
2019-09-06 13:31:43 +0200

    Fix test

--- tests/testthat/test-create_razzo_twinning_params.R
+++ tests/testthat/test-create_razzo_twinning_params.R
@@ -19,7 +19,7 @@ test_that("follows naming convention", {
 test_that("matches article", {
   folder_name <- peregrine::get_pff_tempfile()
   twinning_params <- create_razzo_twinning_params(folder_name = folder_name)
-  expect_true(!beautier::is_one_na(pir_params$twinning_params))
-  expect_equal(pir_params$twinning_params$twin_model, "birth_death")
-  expect_equal(pir_params$twinning_params$method, "random_tree")
+  expect_true(!beautier::is_one_na(twinning_params))
+  expect_equal(twinning_params$twin_model, "birth_death")
+  expect_equal(twinning_params$method, "random_tree")
 })
