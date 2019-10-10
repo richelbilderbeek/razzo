3b256b8e4548d87fd0c2a0d5a03d2d224635adb6
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-10 13:25:36 +0200

    Crown age is 2, progress #330

--- R/get_razzo_crown_age.R
+++ R/get_razzo_crown_age.R
@@ -3,5 +3,5 @@
 #' @author Richel J.C. Bilderbeek
 #' @export
 get_razzo_crown_age <- function() {
-  1.0
+  2.0
 }
--- tests/testthat/test-get_razzo_crown_age.R
+++ tests/testthat/test-get_razzo_crown_age.R
@@ -1,3 +1,3 @@
 test_that("matches article", {
-  expect_equal(get_razzo_crown_age(), 1.0)
+  expect_equal(get_razzo_crown_age(), 2.0)
 })
