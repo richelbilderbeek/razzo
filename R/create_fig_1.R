8dc7ed85be97f20d5fc547e3e0fbe5cb598672d8
G. Laudanno <P274829@workspace.rug.nl>
2019-10-11 19:36:10 +0200

    #276 solved?

--- R/create_fig_1.R
+++ R/create_fig_1.R
@@ -6,10 +6,8 @@
 create_fig_1 <- function(
   project_folder_name = get_razzo_path("razzo_project")
 ) {
-
   check_project_folder_name(project_folder_name)
   df0 <- collect_nltt_stats(project_folder_name)
-
   ##### Satisfy R CMD check #####
   tree <- NULL; rm(tree) # nolint, fixes warning: no visible binding for global variable
   lambda <- NULL; rm(lambda) # nolint, fixes warning: no visible binding for global variable
@@ -23,7 +21,6 @@ create_fig_1 <- function(
   median <- NULL; rm(median) # nolint, fixes warning: no visible binding for global variable
   label_parsed <- NULL; rm(label_parsed) # nolint, fixes warning: no visible binding for global variable
   ..density.. <- NULL; rm(..density..) # nolint, fixes warning: no visible binding for global variable
-
   ##### Data Wrangling #####
   df_errors <- df0
   df_errors$inference_model <- plyr::revalue(
@@ -33,7 +30,6 @@ create_fig_1 <- function(
   df_errors$best_or_gen <- NULL
   names(df_errors) <- gsub(names(df_errors), pattern = "nltt_", replacement = "error_")
   n_errors <- sum(grepl(names(df_errors), pattern = "error_"))
-
   df_params <- collect_mbd_params(project_folder_name)
   df_merged <- merge(x = df_params, y = df_errors, by = "folder")
   col_order <- c(
@@ -47,13 +43,11 @@ create_fig_1 <- function(
     paste0("error_", 1:n_errors)
   )
   df2 <- df_merged[, col_order]
-
   # Convert to long form
   first_col_index <- which(names(df2) == "error_1")
   df_long <- tidyr::gather(
     df2, "error_index", "error_value", first_col_index:ncol(df2)
   )
-
   # Add column tree_and_model, the combination of tree and model type
   df_long$tree_and_model <- interaction(
     df_long$tree,
@@ -64,7 +58,6 @@ create_fig_1 <- function(
   df_long$inference_model <-
     factor(df_long$inference_model, levels = unique(df_long$inference_model))
   rownames(df_long) <- mapply(1:nrow(df_long), FUN = toString)
-
   ##### Theme #####
   label_size <- 13
   label_face <- "italic"
@@ -90,23 +83,21 @@ create_fig_1 <- function(
     axis.text.x = theme_minor_label,
     axis.text.y = theme_minor_label,
     legend.text = theme_minor_label,
-    strip.text.x = ggplot2::element_text(size = 12)
+    strip.text.x = ggplot2::element_text(size = 12),
+    strip.placement = "outside"
   ) +
     ggplot2::theme_bw()
-
   ##### Legend labels #####
   tg_label <- paste("Generative, true.")
   wg_label <- paste("Generative, twin.")
   tb_label <- paste("Best, true.")
   wb_label <- paste("Best, twin.")
-
   tree_and_model_labels <- c(
     tg_label,
     wg_label,
     tb_label,
     wb_label
   )
-
   ##### Facet Labels
   # mu
   mu_values <- unique(df_long$mu)
@@ -120,7 +111,6 @@ create_fig_1 <- function(
   q_values <- unique(df_long$q)
   q_labels <- paste0("q==", q_values) # what you show
   names(q_labels) <- q_values # what's in the df
-
   ##### Fill and line colors #####
   # Line colors: must be darker than the fill color
   tree_and_model_line_colors <- c(
@@ -129,7 +119,6 @@ create_fig_1 <- function(
     "true_best" = "#0000FF", # Blue
     "twin_best" = "#229955"  # Green
   )
-
   # Fill colors: must be lighter than the colors at the edges
   tree_and_model_fill_colors <- c(
     "true_gen" = "#FF3333", # Red
@@ -137,21 +126,18 @@ create_fig_1 <- function(
     "true_best" = "#3333FF", # Blue
     "twin_best" = "#559988"  # Green
   )
-
   ##### Only keep 95% of x axis values #####
   index <- trunc(0.95 * length(df_long$error_value)) # split by mu, nu, q?
   x_top <- sort(df_long$error_value)[index]
-
   ##### More aesthetic settings for the plots #####
   n_error <- length(unique(df_long$error_index))
   bindwidth <- x_top / sqrt(n_error)
   alpha <- 0.5
-
+  bins <- 15
   # Collect the medians
   medians <- df_long %>%
     dplyr::group_by(lambda, mu, nu, q, tree_and_model) %>%
     dplyr::summarise(median = stats::median(error_value))
-
   medians$inference_model <- gsub(
     x = gsub(
       x = medians$tree_and_model,
@@ -177,68 +163,76 @@ create_fig_1 <- function(
     ),
     warn_missing = FALSE
   )
-
   ##### Plot #####
+  ii <- 1
   mus <- unique(df_long$mu)
-  plots_mu <- vector("list", length(mus))
-  for (i_mu in seq_along(mus)) {
-    df_mu <- df_long[df_long$mu == mus[i_mu], ]
-    medians_mu <- medians[medians$mu == mus[i_mu], ]
-    plots_mu[[i_mu]] <-
-      ggplot2::ggplot(
-        data = df_mu,
-        ggplot2::aes(
-          x = error_value,
-          color = tree_and_model,
-          fill = tree_and_model
-        )
-      ) +
-      ggplot2::geom_histogram(
-        data = df_mu,
-        ggplot2::aes(y = bindwidth * ..density..), # nolint the dots in ..density.. are not improper ways to separate words here
-        binwidth = bindwidth,
-        alpha = alpha,
-        position = "identity"
-      ) +
-      ggplot2::facet_grid(
-        nu ~ q,
-        labeller = ggplot2::labeller(
-          mu = ggplot2::as_labeller(mu_labels, ggplot2::label_parsed),
-          nu = ggplot2::as_labeller(nu_labels, ggplot2::label_parsed),
-          q = ggplot2::as_labeller(q_labels, ggplot2::label_parsed)
-        )
-      ) +
-      ggplot2::scale_color_manual(
-        values = tree_and_model_line_colors,
-        labels = tree_and_model_labels
-      ) +
-      ggplot2::scale_fill_manual(
-        values = tree_and_model_fill_colors,
-        labels = tree_and_model_labels
-      ) +
-      ggplot2::scale_x_continuous(
-        minor_breaks = seq(0.0, 1.0, 0.01)
-      ) +
-      ggplot2::coord_cartesian(
-        xlim = c(min(df_mu$error_value), x_top)
-      ) +
-      ggplot2::geom_vline(
-        data = medians_mu,
-        ggplot2::aes(
-          xintercept = median,
-          color = tree_and_model
-        ),
-        linetype = "dashed"
-      ) +
-      ggplot2::ggtitle(paste0(
-        "Inference error distribution: mu = ", mus[i_mu]
-      )) +
-      ggplot2::labs(
-        x = "Error",
-        y = "Density",
-        fill = "Model and tree",
-        color = "Model and tree"
-      ) + theme
+  plots <- vector("list", length(mus))
+  mts <- unique(df_long$inference_model) # model_types
+  for (i_mt in seq_along(mts)) {
+    df_mt <- df_long[df_long$inference_model == mts[i_mt], ]
+    medians_mt <- medians[medians$inference_model == mts[i_mt], ]
+    for (i_mu in seq_along(mus)) {
+      df_mu <- df_mt[df_mt$mu == mus[i_mu], ]
+      medians_mu <- medians_mt[medians_mt$mu == mus[i_mu], ]
+      plots[[ii]] <-
+        ggplot2::ggplot(
+          data = df_mu,
+          ggplot2::aes(
+            x = error_value,
+            color = tree_and_model,
+            fill = tree_and_model
+          )
+        ) +
+        ggplot2::geom_histogram(
+          data = df_mu,
+          ggplot2::aes(y = bindwidth * ..density..), # nolint the dots in ..density.. are not improper ways to separate words here
+          bins = bins,
+          alpha = alpha,
+          position = "identity"
+        ) +
+        ggplot2::facet_wrap(
+          nu ~ q,
+          labeller = ggplot2::labeller(
+            mu = ggplot2::as_labeller(mu_labels, ggplot2::label_parsed),
+            nu = ggplot2::as_labeller(nu_labels, ggplot2::label_parsed),
+            q = ggplot2::as_labeller(q_labels, ggplot2::label_parsed)
+          ),
+          strip.position = "top",
+          scales = "free",
+          ncol = length(q_values)
+        ) +
+        ggplot2::scale_color_manual(
+          values = tree_and_model_line_colors,
+          labels = tree_and_model_labels
+        ) +
+        ggplot2::scale_fill_manual(
+          values = tree_and_model_fill_colors,
+          labels = tree_and_model_labels
+        ) +
+        ggplot2::geom_vline(
+          data = medians_mu,
+          ggplot2::aes(
+            xintercept = median,
+            color = tree_and_model
+          ),
+          linetype = "dashed"
+        ) +
+        ggplot2::ggtitle(paste0(
+          "Inference error distribution: lambda = ",
+          unique(df_mu$lambda),
+          ", mu = ",
+          mus[i_mu],
+          ", crown age = ",
+          unique(df_mu$crown_age)
+        )) +
+        ggplot2::labs(
+          x = "Error",
+          y = "Density",
+          fill = "Model and tree",
+          color = "Model and tree"
+        ) + theme
+      ii <- ii + 1
+    }
   }
-  plots_mu
+  plots
 }
