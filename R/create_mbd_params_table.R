1ff280c2b8b7a045e603e783474625611a9ad2f7
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-12 11:11:26 +0200

    Copied 'mbd.TaxaAndMbness::create_params_table' to 'create_mbd_params_table', due to https://github.com/Giappo/mbd.TaxaAndMbness/issues/1

--- R/create_mbd_params_table.R
+++ R/create_mbd_params_table.R
@@ -28,74 +28,25 @@
 #' expect_true("seed" %in% names(df))
 #' @export
 create_mbd_params_table <- function(
-  lambda = 0.2,
-  mu = c(0, 0.15),
-  nu = get_razzo_nus(),
-  q = c(0.1, 0.15, 0.2),
-  n_replicates = get_razzo_n_replicates(),
-  crown_age = get_razzo_crown_age(),
-  cond = 1
+  lambdas = c(0.2),
+  mus = c(0, 0.1),
+  nus = c(0, 0.5, 1.0, 1.5),
+  qs = c(0.1, 0.15, 0.2),
+  cond = 1,
+  crown_age = 8,
+  n_replicates = 2
 ) {
-  lambda <- unique(lambda)
-  mu <- unique(mu)
-  nu <- unique(nu)
-  q <- unique(q)
-  seed <- 1:n_replicates
-  mbd_params_table <- expand.grid(
-    seed = seed,
-    lambda = lambda,
-    mu = mu,
-    nu = nu,
-    q = q,
-    crown_age = crown_age,
-    cond = cond
-  )
-  seeds <- 1:nrow(mbd_params_table)
-  mbd_params_table$seed <- NULL
-  mbd_params_table$seed <- seeds
-  testit::assert(
-   length(crown_age) == 1
-  )
-  testit::assert(
-   length(cond) == 1
-  )
-  mbd_params_table
-}
 
-#' Create a table of which each row holds a unique MBD parameter combination.
-#' @inheritParams default_params_doc
-#' @seealso use \link{create_mbd_paramses} to create a list
-#' of \code{mbd_params}
-#' @return a data frame with the following columns:
-#' \itemize{
-#'   \item lambda the speciation rate
-#'   \item mu the extinction rate
-#'   \item nu the co-occurring speciation event rate
-#'   \item q the proportion of species that speciates at a co-occurring
-#'     speciation event
-#'   \item crown_age the crown age of the MBD tree
-#'   \item cond the conditioning
-#'   \item seed the RNG seed for creating an MBD tree
-#' }
-#' @author Giovanni Laudanno, Richel J.C. Bilderbeek
-#' @examples
-#' library(testthat)
-#'
-#' df <- create_mbd_params_table()
-#' expect_true(is.data.frame(df))
-#' expect_equal(nrow(unique(df)), nrow(df))
-#' expect_true("lambda" %in% names(df))
-#' expect_true("mu" %in% names(df))
-#' expect_true("nu" %in% names(df))
-#' expect_true("q" %in% names(df))
-#' expect_true("crown_age" %in% names(df))
-#' expect_true("seed" %in% names(df))
-#' @export
-create_mbd_params_table_giappo <- function(
-  n_replicates = get_razzo_n_replicates()
-) {
-  # Issue #336, Issue 336
-  mbd.TaxaAndMbness::create_params_table(
-    n_replicates = n_replicates
-  )
+  x <- expand.grid(lambda = lambdas, mu = mus, nu = nus, q = qs)
+  no_mbd_lines <-
+    apply(X = x, MARGIN = 1, FUN = function(y) y[3] == 0 | y[4] == 0)
+  no_mbd_x <- expand.grid(lambda = lambdas, mu = mus, nu = 0, q = 0)
+  x[no_mbd_lines, ] <- no_mbd_x
+  x <- dplyr::distinct(x)
+  x$crown_age <- crown_age
+  x$cond <- cond
+  x2 <- x[rep(1:nrow(x), rep(n_replicates, nrow(x))), ]
+  x2$seed <- 1:nrow(x2)
+  rownames(x2) <- NULL
+  x2
 }
