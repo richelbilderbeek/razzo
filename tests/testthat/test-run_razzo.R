1ec3287b7577cf3372283352e3c3059f554049a7
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-15 11:25:50 +0200

    Check that MCMC chain length matches the final MCMC state number of the BEAST2 output file, progress #350

--- tests/testthat/test-run_razzo.R
+++ tests/testthat/test-run_razzo.R
@@ -6,4 +6,28 @@ test_that("use", {
 
   razzo_params <- create_test_razzo_params()
   run_razzo(razzo_params)
+
+  # Check if MCMC chain length in parameters matches the final state
+  # in the BEAST2 output files
+  expected_mcmc_chain_length <-
+    razzo_params$pir_params$experiments[[1]]$inference_model$mcmc$chain_length
+
+  final_mcmc_state_line <- tail(
+    readLines(
+      razzo_params$pir_params$experiments[[1]]$beast2_options$output_trees_filenames, # nolint sorry Demeter
+      warn = FALSE
+    ),
+    n = 2
+  )[1]
+  final_mcmc_state_line
+  actual_mcmc_chain_length <- as.numeric(
+    stringr::str_match(
+      string = final_mcmc_state_line,
+      pattern = "tree STATE_(.*) = \\("
+    )[1, 2]
+  )
+  expect_equal(
+    expected_mcmc_chain_length,
+    actual_mcmc_chain_length
+  )
 })
