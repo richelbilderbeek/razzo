f9c10520c872042b26cb07115b19d2602b50300c
richelbilderbeek <richel@richelbilderbeek.nl>
2019-10-29 17:03:13 +0100

    Progress ...

---
+++ R/razzo.R
@@ -0,0 +1,7 @@
+#' razzo: the effect of MBD on inference
+#'
+#' @docType package
+#' @name razzo
+#' @import beautier, tracerer, beastier, tracerer, babette, mcbette, nodeSub 
+#' @import pirouette, peregrine
+NULL
