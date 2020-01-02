af18656faf94ff0b96f873e7faefb4d5633e3502
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-02 08:03:53 +0100

    Doc and test 'check_traces_in_df'

--- R/check_traces_in_df.R
+++ R/check_traces_in_df.R
@@ -1,4 +1,13 @@
 #' Check if all the traces' names are in the names of the data frames
+#' @param traces_names names of the traces
+#' @param df a data frame
+#' @author Richel J.C. Bilderbeek
+#' @examples
+#' library(testthat)
+#'
+#' df <- data.frame(my_name = 3.14)
+#' expect_silent(check_traces_in_df("my_name", df))
+#' expect_error(check_traces_in_df("absent", df))
 #' @export
 check_traces_in_df <- function(traces_names, df) {
   if (!all(traces_names %in% names(df))) {
