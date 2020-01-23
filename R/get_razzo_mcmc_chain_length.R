f64bfc3123fb61723ffdd9311f059d33d5e3dde4
richelbilderbeek <richel@richelbilderbeek.nl>
2020-01-23 11:35:59 +0100

    'get_razzo_mcmc_chain_length' matches the article ('1e+07') again

--- R/get_razzo_mcmc_chain_length.R
+++ R/get_razzo_mcmc_chain_length.R
@@ -3,7 +3,5 @@
 #' @author Richel J.C. Bilderbeek
 #' @export
 get_razzo_mcmc_chain_length <- function() {
-  # Changed for https://github.com/richelbilderbeek/razzo/issues/346
-  # Original value: 1e7
-  1e8
+  1e7
 }
--- tests/testthat/test-get_razzo_mcmc_chain_length.R
+++ tests/testthat/test-get_razzo_mcmc_chain_length.R
@@ -1,6 +1,4 @@
-test_that("matches #346", {
-  skip("Changed for https://github.com/richelbilderbeek/razzo/issues/346")
-
+test_that("matches article", {
   expect_equal(
     1e7,
     get_razzo_mcmc_chain_length()
